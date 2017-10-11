95c0e352232efb1677ae669054f5c0b55a7d595a
piyush <piyush.upadhyay@godoctor.in>
2017-10-11 12:13:26 +0530

    run time permission done

--- app/src/main/java/com/example/piyush/qrcodereadergooglevisionapi/MainActivity.java
+++ app/src/main/java/com/example/piyush/qrcodereadergooglevisionapi/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.piyush.qrcodereadergooglevisionapi;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +12,7 @@ import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -22,11 +26,13 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_FOR_CAMERA = 101;
     private  final String TAG = getClass().getSimpleName();
     SurfaceView mCameraView;
     BarcodeDetector mBarcodeDetector;
     CameraSource mCameraSource;
     SurfaceHolder mSurfaceHolder;
+    TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,28 +40,40 @@ public class MainActivity extends AppCompatActivity {
         mCameraView = findViewById(R.id.cameraView);
         mCameraView.setZOrderMediaOverlay(true);
         mSurfaceHolder = mCameraView.getHolder();
-
+        mTextView=findViewById(R.id.textView);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        // check for google play service
-        if(isGooglePlayServicesAvailable(MainActivity.this)) {
-            // always setup in onResume only
-            setup();
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED) {
+           initialSetUp();
         }else{
-            // take user to installation for google play services..
-            Log.e(TAG, "onResume: " );
+//            take run time permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    MY_PERMISSIONS_REQUEST_FOR_CAMERA);
         }
     }
 
-    private void setup() {
+    private void initialSetUp() {
+        // check for google play service
+        if (isGooglePlayServicesAvailable(MainActivity.this)) {
+            // always finalSetup in onResume only
+            finalSetup();
+        } else {
+            // take user to installation for google play services..
+            Log.e(TAG, "onResume: ");
+        }
+    }
+
+    private void finalSetup() {
         mBarcodeDetector = new BarcodeDetector.Builder(this)
                 .setBarcodeFormats(Barcode.QR_CODE)
                 .build();
         if(!mBarcodeDetector.isOperational()){
-            Toast.makeText(getApplicationContext(), "Couldn't setup the detector", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Couldn't Setup the detector", Toast.LENGTH_LONG).show();
             this.finish();
         }
         mCameraSource = new CameraSource.Builder(this, mBarcodeDetector)
@@ -100,7 +118,7 @@ public class MainActivity extends AppCompatActivity {
                 if(barcodes.size() > 0){
                     MainActivity.this.runOnUiThread(new Runnable() {
                         public void run() {
-                            Toast.makeText(MainActivity.this,  barcodes.valueAt(0).displayValue, Toast.LENGTH_SHORT).show();
+                            mTextView.setText(barcodes.valueAt(0).displayValue);
 
                         }
                     });
@@ -112,6 +130,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        if(mCameraSource!=null)
         mCameraSource.release();
     }
 
@@ -125,5 +144,32 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
         return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_FOR_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+//                    Just refreshing this activity you can bulid your own logic....
+
+                    Intent ss = new Intent(this, MainActivity.class);
+                    ss.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                    ss.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    ss.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    ss.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(ss);
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Grant camera permission ", Toast.LENGTH_SHORT).show();
+                }
+
+                return;
+            }
+
+        }
     }
 }
